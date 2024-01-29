5c920474e50d31a3b8a16ef84ef27bcee3ca7284
Denys Doroshenko <denisdoroshenko99@gmail.com>
2024-01-29 23:46:00 +0200

    add Header component, PrivateRoute util

--- spiky_frontend/src/App.js
+++ spiky_frontend/src/App.js
@@ -1,14 +1,18 @@
 import './App.css';
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import PrivateRoute from './utils/PrivateRoute';
+
 import HomePage from './pages/HomePage';
 import LoginPage from './pages/LoginPage';
+import Header from './components/Header'
 
 function App() {
   return (
     <div className="App">
       <BrowserRouter>
+        <Header />
         <Routes>
-          <Route element={<HomePage />} path='/' exact />
+          <Route element={<PrivateRoute component={HomePage} />} path='/*' exact />
           <Route element={<LoginPage />} path='/login' />
         </Routes>
       </BrowserRouter>
