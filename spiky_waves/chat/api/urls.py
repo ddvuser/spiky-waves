d76fda8bd2ed13bdf6582d4a72782bcb8a0cae85
Denys Doroshenko <denisdoroshenko99@gmail.com>
2024-02-02 02:37:45 +0200

    add media url to root urls

--- spiky_waves/chat/api/urls.py
+++ spiky_waves/chat/api/urls.py
@@ -3,7 +3,7 @@ from . import views
 
 urlpatterns = [
     path('my-messages/<user_id>/', views.MyMessages.as_view()),
-    path('get-messages/<sender_id>/<receiver_id>', views.GetMessages.as_view()),
+    path('get-messages/<sender_id>/<receiver_id>/', views.GetMessages.as_view()),
     path('send-messages/', views.SendMessage.as_view()),
     path('profile/<int:pk>/', views.ProfileDetail.as_view()),
     path('search-user/<str:username>/', views.SearchUser.as_view()),
--- spiky_waves/spiky_waves/urls.py
+++ spiky_waves/spiky_waves/urls.py
@@ -15,9 +15,13 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.conf.urls.static import static
+from django.conf import settings
 
 urlpatterns = [
     path('auth/api/', include('authentication.api.urls')),
     path('chat/api/', include('chat.api.urls')),
     path('admin/', admin.site.urls),
 ]
+
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
