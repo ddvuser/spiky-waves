deac010e76a7d72366f558f2d615ea1104dcc33f
Denys Doroshenko <denisdoroshenko99@gmail.com>
2024-02-02 15:59:25 +0200

    change Header design, add bootstrap JS

--- spiky_frontend/src/components/Header.js
+++ spiky_frontend/src/components/Header.js
@@ -1,24 +1,39 @@
-import React, { useContext } from 'react'
-import { Link } from 'react-router-dom'
-import AuthContext from '../context/AuthContext'
+import React, { useContext } from 'react';
+import { Link } from 'react-router-dom';
+import AuthContext from '../context/AuthContext';
 
 const Header = () => {
-  let {user, logoutUser} = useContext(AuthContext)
+  const { user, logoutUser } = useContext(AuthContext);
 
   return (
-    <nav>
-      <ul>
-        <li><Link to="/">Home</Link></li>
-          {user ? 
-            (<li onClick={logoutUser}>Logout</li>)
-            :
-            (<li><Link to="/login">Login</Link></li>)
-          }
-
-          {user && <li>logged in as {user.email}</li>}
-      </ul>
+    <nav className="navbar navbar-expand-lg navbar-light bg-light">
+      <Link to="/" className="navbar-brand">SpikyWaves</Link>
+      <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+        <span className="navbar-toggler-icon"></span>
+      </button>
+      <div className="collapse navbar-collapse" id="navbarNav">
+        <ul className="navbar-nav ml-auto">
+          <li className="nav-item">
+            <Link to="/" className="nav-link">Home</Link>
+          </li>
+          {user ? (
+            <li className="nav-item" onClick={logoutUser}>
+              <span className="nav-link" style={{ cursor: 'pointer' }}>Logout</span>
+            </li>
+          ) : (
+            <li className="nav-item">
+              <Link to="/login" className="nav-link">Login</Link>
+            </li>
+          )}
+          {user && (
+            <li className="nav-item">
+              <span className="nav-link">Logged in as {user.email}</span>
+            </li>
+          )}
+        </ul>
+      </div>
     </nav>
-  )
-}
+  );
+};
 
 export default Header;
