27556636dd882780d12660d8c07bcba598bcf0ba
Denys Doroshenko <denisdoroshenko99@gmail.com>
2024-02-03 21:05:00 +0200

    change LoginPage design

--- spiky_frontend/src/pages/LoginPage.js
+++ spiky_frontend/src/pages/LoginPage.js
@@ -5,12 +5,22 @@ const LoginPage = () => {
     let {loginUser} = useContext(AuthContext);
 
     return (
-        <div>
-            <form onSubmit={loginUser}>
-                <input type='text' name='email' placeholder='Enter an e-mail' />
-                <input type='text' name='password' placeholder='Enter a password' />
-                <input type='submit' />
-            </form>
+        <div className='row'>
+            <div className='col'></div>
+            <div className='col-6'>
+                <form onSubmit={loginUser}>
+                    <div className='form-group mt-2 mb-2'>
+                        <label className='mb-1' htmlFor='login-email'>Email address:</label>
+                        <input className='form-control' type='text' name='email' id='login-email' placeholder='name@example.com' />
+                    </div> 
+                    <div className='form-group'>
+                        <label className='mb-1' htmlFor='login-password'>Password:</label>
+                        <input className='form-control' type='password' name='password' id='login-password' placeholder='...' />
+                    </div>
+                    <button type="submit" className="btn btn-primary mt-2">Login</button>
+                </form>
+            </div>
+            <div className='col'></div>
         </div>
     )
 }
