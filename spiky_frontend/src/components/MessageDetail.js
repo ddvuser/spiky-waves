07b03252fd8a38e3ec4c0f5069fcf106c9de69de
Denys Doroshenko <denisdoroshenko99@gmail.com>
2024-02-08 14:15:46 +0200

    add timestamp to sender messages

--- spiky_frontend/src/components/MessageDetail.js
+++ spiky_frontend/src/components/MessageDetail.js
@@ -1,39 +1,54 @@
-import React from 'react';
-import moment from 'moment';
+import React from "react";
+import moment from "moment";
 
 const MessageItem = ({ message, user }) => {
   return (
     <div key={message.id} className="list-group-item border-0">
       <div>
         {message.sender.id !== user.user_id ? (
-          <div className='d-flex align-items-center flex-row'>
-            <img
-              src={message.sender_profile.image}
-              className="rounded-circle me-2"
-              alt="1"
-              width={40}
-              height={40}
-            />
-            <div>
-              {message.sender.name !== null && message.sender.surname !== null
-                ? `${message.sender.name} ${message.sender.surname}`
-                : message.sender.username}
+          <div>
+            <div className="d-flex justify-content-center">
+              <small>
+                <span className="badge bg-warning badge-pill text-white">
+                  {moment
+                    .utc(message.created)
+                    .local()
+                    .format("MMMM Do YYYY, h:mm:ss a")}
+                </span>
+              </small>
+            </div>
+            <div className="d-flex align-items-center flex-row">
+              <img
+                src={message.sender_profile.image}
+                className="rounded-circle me-2"
+                alt="1"
+                width={40}
+                height={40}
+              />
               <div>
-                <p style={{ overflowWrap: 'break-word' }}>{message.text}</p>
+                {message.sender.name !== null && message.sender.surname !== null
+                  ? `${message.sender.name} ${message.sender.surname}`
+                  : message.sender.username}
+                <div>
+                  <p style={{ overflowWrap: "break-word" }}>{message.text}</p>
+                </div>
               </div>
             </div>
           </div>
         ) : (
           <div>
-            <div className='d-flex justify-content-center'>
+            <div className="d-flex justify-content-center">
               <small>
-                <span className="badge bg-light badge-pill text-white">
-                  {moment.utc(message.created).local().format('MMMM Do YYYY, h:mm:ss a')}
+                <span className="badge bg-info badge-pill text-white">
+                  {moment
+                    .utc(message.created)
+                    .local()
+                    .format("MMMM Do YYYY, h:mm:ss a")}
                 </span>
-              </small>  
+              </small>
             </div>
-            <div className='d-flex flex-row-reverse'>
-              <div className='d-flex align-items-center flex-row-reverse'>
+            <div className="d-flex flex-row-reverse">
+              <div className="d-flex align-items-center flex-row-reverse">
                 <img
                   src={message.receiver_profile.image}
                   className="rounded-circle mr-1"
@@ -42,12 +57,10 @@ const MessageItem = ({ message, user }) => {
                   height={40}
                 />
               </div>
-              <div className='d-flex flex-column me-2'>
-                <div className='d-flex flex-row-reverse'>
-                  me:
-                </div>
-                <div className='text-break'>
-                  <p style={{ overflowWrap: 'break-word' }}>{message.text}</p>
+              <div className="d-flex flex-column me-2">
+                <div className="d-flex flex-row-reverse">me:</div>
+                <div className="text-break">
+                  <p style={{ overflowWrap: "break-word" }}>{message.text}</p>
                 </div>
               </div>
             </div>
@@ -59,4 +72,3 @@ const MessageItem = ({ message, user }) => {
 };
 
 export default MessageItem;
-
