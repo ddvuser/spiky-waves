8c922afb8d733a52d0b8adcbbeb433a7c8d886ca
Denys Doroshenko <denisdoroshenko99@gmail.com>
2024-02-17 20:23:13 +0200

    add password reset serializers

--- spiky_waves/authentication/api/serializers.py
+++ spiky_waves/authentication/api/serializers.py
@@ -1,10 +1,12 @@
 from rest_framework import serializers
 from authentication.models import CustomUser
 
+
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = CustomUser
-        fields = ['email', 'username', 'name', 'surname', 'phone', 'password']
+        fields = ["email", "username", "name", "surname", "phone", "password"]
+
 
 class ChangePasswordSerializer(serializers.Serializer):
     current_password = serializers.CharField(required=True)
@@ -12,12 +14,31 @@ class ChangePasswordSerializer(serializers.Serializer):
     confirm_new_password = serializers.CharField(required=True)
 
     def validate(self, data):
-        current_password = data.get('current_password')
-        new_password = data.get('new_password')
-        confirm_new_password = data.get('confirm_new_password')
+        current_password = data.get("current_password")
+        new_password = data.get("new_password")
+        confirm_new_password = data.get("confirm_new_password")
 
         # Check if new password matches the confirmation
         if new_password != confirm_new_password:
-            raise serializers.ValidationError("New password and confirmation do not match")
+            raise serializers.ValidationError(
+                "New password and confirmation do not match"
+            )
 
         return data
+
+
+class ResetPasswordRequestSerializer(serializers.Serializer):
+    email = serializers.EmailField(required=True)
+
+
+class ResetPasswordSerializer(serializers.Serializer):
+    new_password = serializers.RegexField(
+        regex=r"^(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$",
+        write_only=True,
+        error_messages={
+            "invalid": (
+                "Password must be at least 8 characters long with at least one capital letter and symbol"
+            )
+        },
+    )
+    confirm_password = serializers.CharField(write_only=True, required=True)
--- spiky_waves/authentication/api/urls.py
+++ spiky_waves/authentication/api/urls.py
@@ -5,8 +5,20 @@ from rest_framework_simplejwt.views import (
 )
 
 urlpatterns = [
-    path('token/', views.MyTokenObtainPairView.as_view(), name='token_obtain_pair'),
-    path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
-    path('create-user/', views.UserCreateView.as_view(), name='create-user'),
-    path('change-password/<int:pk>/', views.ChangePassword.as_view(), name='change-password'),
+    path("token/", views.MyTokenObtainPairView.as_view(), name="token_obtain_pair"),
+    path("token/refresh/", TokenRefreshView.as_view(), name="token_refresh"),
+    path("create-user/", views.UserCreateView.as_view(), name="create-user"),
+    path(
+        "change-password/<int:pk>/",
+        views.ChangePassword.as_view(),
+        name="change-password",
+    ),
+    path(
+        "init-reset-pass/", views.RequestPasswordReset.as_view(), name="init-reset-pass"
+    ),
+    path(
+        "confirm-pass-reset/<str:token>/",
+        views.ResetPassword.as_view(),
+        name="confirm-pass-reset",
+    ),
 ]
