998c51d228cfedecb7007c9fdfe117493fb9e002
Denys Doroshenko <denisdoroshenko99@gmail.com>
2024-02-17 02:13:39 +0200

    adapt Login and Register pages for small screens

--- spiky_frontend/src/pages/LoginPage.js
+++ spiky_frontend/src/pages/LoginPage.js
@@ -1,28 +1,64 @@
-import React, { useContext } from 'react'
-import AuthContext from '../context/AuthContext'
+import React, { useContext, useEffect, useState } from "react";
+import AuthContext from "../context/AuthContext";
+import { Link } from "react-router-dom";
 
 const LoginPage = () => {
-    let {loginUser} = useContext(AuthContext);
+  let { loginUser } = useContext(AuthContext);
 
-    return (
-        <div className='row'>
-            <div className='col'></div>
-            <div className='col-6'>
-                <form onSubmit={loginUser}>
-                    <div className='form-group mt-2 mb-2'>
-                        <label className='mb-1' htmlFor='login-email'>Email address:</label>
-                        <input className='form-control' type='text' name='email' id='login-email' placeholder='name@example.com' />
-                    </div> 
-                    <div className='form-group'>
-                        <label className='mb-1' htmlFor='login-password'>Password:</label>
-                        <input className='form-control' type='password' name='password' id='login-password' placeholder='...' />
-                    </div>
-                    <button type="submit" className="btn btn-primary mt-2">Login</button>
-                </form>
-            </div>
-            <div className='col'></div>
-        </div>
-    )
-}
+  let [isSmallScreen, setIsSmallScreen] = useState(window.innerWidth <= 768);
 
-export default LoginPage
+  useEffect(() => {
+    const handleResize = () => {
+      setIsSmallScreen(window.innerWidth <= 768);
+    };
+
+    window.addEventListener("resize", handleResize);
+
+    return () => {
+      window.removeEventListener("resize", handleResize);
+    };
+  }, []);
+
+  return (
+    <div className="row">
+      {!isSmallScreen && <div className="col"></div>}
+      <div className={!isSmallScreen ? "col-6" : "col ms-1 me-1"}>
+        <form onSubmit={loginUser}>
+          <div className="form-group mt-2 mb-2">
+            <label className="mb-1" htmlFor="login-email">
+              Email address:
+            </label>
+            <input
+              className="form-control"
+              type="text"
+              name="email"
+              id="login-email"
+              placeholder="name@example.com"
+            />
+          </div>
+          <div className="form-group">
+            <label className="mb-1" htmlFor="login-password">
+              Password:
+            </label>
+            <input
+              className="form-control"
+              type="password"
+              name="password"
+              id="login-password"
+              placeholder="..."
+            />
+          </div>
+          <div className="form-group">
+            <Link to="/init-pass-reset">Forgot your password?</Link>
+          </div>
+          <button type="submit" className="btn btn-primary mt-2">
+            Login
+          </button>
+        </form>
+      </div>
+      {!isSmallScreen && <div className="col"></div>}
+    </div>
+  );
+};
+
+export default LoginPage;
--- spiky_frontend/src/pages/RegisterPage.js
+++ spiky_frontend/src/pages/RegisterPage.js
@@ -1,56 +1,124 @@
-import React, { useContext } from 'react'
-import AuthContext from '../context/AuthContext'
+import React, { useContext, useState, useEffect } from "react";
+import AuthContext from "../context/AuthContext";
 
 function RegisterPage() {
-    let {registerUser} = useContext(AuthContext);
+  let { registerUser } = useContext(AuthContext);
+  let [isSmallScreen, setIsSmallScreen] = useState(window.innerWidth <= 768);
 
-    return (
-        <div className='row'>
-            <div className='col'></div>
-            <div className='col-6'>
-                <form onSubmit={registerUser}>
-                    <div className='form-group mt-2 mb-2'>
-                        <label className='mb-1' htmlFor='register-'>Email address:</label>
-                        <input className='form-control' type='text' name='email' id='register-email' placeholder='name@example.com' />
-                    </div>
-                    <div className='form-group mt-2 mb-2'>
-                        <label className='mb-1' htmlFor='register-username'>Username:</label>
-                        <input className='form-control' type='text' name='username' id='register-email' placeholder='...' />
-                    </div>
-                    {/* Name, Surname, Phone */}
-                    <div className='form-group'>
-                        <label className='mb-1' htmlFor='register-name'>Name:
-                            <small className='form-text text-muted'>(optional)</small>
-                        </label>
-                        <input className='form-control' type='text' name='name' id='register-name' placeholder='...' />
-                    </div>
-                    <div className='form-group'>
-                        <label className='mb-1' htmlFor='register-surname'>Surname:
-                            <small className='form-text text-muted'>(optional)</small>
-                        </label>
-                        <input className='form-control' type='text' name='surname' id='register-surname' placeholder='...' />
-                    </div>
-                    <div className='form-group'>
-                        <label className='mb-1' htmlFor='register-phone'>Phone:
-                            <small className='form-text text-muted'>(optional)</small>
-                        </label>
-                        <input className='form-control' type='text' name='phone' id='register-phone' placeholder='(415) 555‑0132' />
-                    </div>
-                    {/* Password */}
-                    <div className='form-group'>
-                        <label className='mb-1' htmlFor='register-password1'>Password:</label>
-                        <input className='form-control' type='password' name='password' id='register-password1' placeholder='...' />
-                    </div>
-                    <div className='form-group'>
-                        <label className='mb-1' htmlFor='register-password2'>Password confirmation:</label>
-                        <input className='form-control' type='password' name='password2' id='register-password2' placeholder='...' />
-                    </div>
-                    <button type="submit" className="btn btn-primary mt-2">Register</button>
-                </form>
-            </div>
-            <div className='col'></div>
-        </div>
-    )
+  useEffect(() => {
+    const handleResize = () => {
+      setIsSmallScreen(window.innerWidth <= 768);
+    };
+
+    window.addEventListener("resize", handleResize);
+
+    return () => {
+      window.removeEventListener("resize", handleResize);
+    };
+  }, []);
+
+  return (
+    <div className="row">
+      {!isSmallScreen && <div className="col"></div>}
+      <div className={!isSmallScreen ? "col-6" : "col ms-1 me-1"}>
+        <form onSubmit={registerUser}>
+          <div className="form-group mt-2 mb-2">
+            <label className="mb-1" htmlFor="register-">
+              Email address:
+            </label>
+            <input
+              className="form-control"
+              type="text"
+              name="email"
+              id="register-email"
+              placeholder="name@example.com"
+            />
+          </div>
+          <div className="form-group mt-2 mb-2">
+            <label className="mb-1" htmlFor="register-username">
+              Username:
+            </label>
+            <input
+              className="form-control"
+              type="text"
+              name="username"
+              id="register-email"
+              placeholder="..."
+            />
+          </div>
+          {/* Name, Surname, Phone */}
+          <div className="form-group">
+            <label className="mb-1" htmlFor="register-name">
+              Name:
+              <small className="form-text text-muted">(optional)</small>
+            </label>
+            <input
+              className="form-control"
+              type="text"
+              name="name"
+              id="register-name"
+              placeholder="..."
+            />
+          </div>
+          <div className="form-group">
+            <label className="mb-1" htmlFor="register-surname">
+              Surname:
+              <small className="form-text text-muted">(optional)</small>
+            </label>
+            <input
+              className="form-control"
+              type="text"
+              name="surname"
+              id="register-surname"
+              placeholder="..."
+            />
+          </div>
+          <div className="form-group">
+            <label className="mb-1" htmlFor="register-phone">
+              Phone:
+              <small className="form-text text-muted">(optional)</small>
+            </label>
+            <input
+              className="form-control"
+              type="text"
+              name="phone"
+              id="register-phone"
+              placeholder="(415) 555‑0132"
+            />
+          </div>
+          {/* Password */}
+          <div className="form-group">
+            <label className="mb-1" htmlFor="register-password1">
+              Password:
+            </label>
+            <input
+              className="form-control"
+              type="password"
+              name="password"
+              id="register-password1"
+              placeholder="..."
+            />
+          </div>
+          <div className="form-group">
+            <label className="mb-1" htmlFor="register-password2">
+              Password confirmation:
+            </label>
+            <input
+              className="form-control"
+              type="password"
+              name="password2"
+              id="register-password2"
+              placeholder="..."
+            />
+          </div>
+          <button type="submit" className="btn btn-primary mt-2">
+            Register
+          </button>
+        </form>
+      </div>
+      {!isSmallScreen && <div className="col"></div>}
+    </div>
+  );
 }
 
 export default RegisterPage;
