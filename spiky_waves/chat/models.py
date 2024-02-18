5bf241da220f44fb43429cdad4b4667b26eaa668
Denys Doroshenko <denisdoroshenko99@gmail.com>
2024-02-18 20:52:11 +0200

    format models module

--- spiky_waves/chat/models.py
+++ spiky_waves/chat/models.py
@@ -1,30 +1,38 @@
 from django.db import models
 from authentication.models import CustomUser
 
+
 class Profile(models.Model):
     user = models.OneToOneField(CustomUser, on_delete=models.CASCADE)
     full_name = models.CharField(max_length=49)
     bio = models.CharField(max_length=100)
-    image = models.ImageField(upload_to="uploads/profile_images/", default='default_profile_pic.png')
+    image = models.ImageField(
+        upload_to="uploads/profile_images/", default="default_profile_pic.png"
+    )
     verified = models.BooleanField(default=False)
 
     def __str__(self) -> str:
         return f"{self.user.email} - {self.full_name}"
 
+
 class Message(models.Model):
-    sender = models.ForeignKey(CustomUser, on_delete=models.CASCADE, related_name='sender')
-    receiver = models.ForeignKey(CustomUser, on_delete=models.CASCADE, related_name='receiver')
+    sender = models.ForeignKey(
+        CustomUser, on_delete=models.CASCADE, related_name="sender"
+    )
+    receiver = models.ForeignKey(
+        CustomUser, on_delete=models.CASCADE, related_name="receiver"
+    )
     text = models.CharField(max_length=1000)
     created = models.DateTimeField(auto_now_add=True)
     updated = models.DateTimeField(auto_now=True)
     is_read = models.BooleanField(default=False)
 
     class Meta:
-        ordering = ['created']
-    
+        ordering = ["created"]
+
     def __str__(self) -> str:
         return f"{self.sender} to {self.receiver}"
-    
+
     @property
     def sender_profile(self):
         sender_profile = Profile.objects.get(user=self.sender)
@@ -34,4 +42,3 @@ class Message(models.Model):
     def receiver_profile(self):
         receiver_profile = Profile.objects.get(user=self.receiver)
         return receiver_profile
-    
