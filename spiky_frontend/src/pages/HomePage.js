f129a8c80dd52a06e4251eeca510d32c8513f48f
Denys Doroshenko <denisdoroshenko99@gmail.com>
2024-02-20 02:50:58 +0200

    optimize HomePage get messages request

--- spiky_frontend/src/pages/HomePage.js
+++ spiky_frontend/src/pages/HomePage.js
@@ -6,22 +6,19 @@ import SearchResults from "../components/SearchResults";
 import Loading from "../components/Loading";
 import InboxPage from "./InboxPage";
 import "./HomePage.css";
+import { useAxios } from "../hooks/useAxios";
 
 const HomePage = () => {
-  let [messages, setMessages] = useState([]);
-  let { user, authTokens, logoutUser } = useContext(AuthContext);
+  let [latestMessages, setMessages] = useState([]);
+  let { user, authTokens } = useContext(AuthContext);
 
   let [chatParticipants, setChatParticipants] = useState(null);
   let [selectedProfile, setSelectedProfile] = useState(null);
   let [isSmallScreen, setIsSmallScreen] = useState(window.innerWidth <= 768);
 
-  let [isLoading, setIsLoading] = useState(true);
+  let [loading, setLoading] = useState(true);
 
   let [searchResult, setSearchResult] = useState(null);
-
-  useEffect(() => {
-    myMessages();
-  }, [chatParticipants]);
 
   useEffect(() => {
     const handleResize = () => {
@@ -35,26 +32,22 @@ const HomePage = () => {
     };
   }, []);
 
-  let myMessages = async () => {
-    console.log("Fetch my-messages");
-    let response = await fetch(
-      `http://127.0.0.1:8000/chat/api/my-messages/${user.user_id}/`,
-      {
-        method: "GET",
-        headers: {
-          "Content-Type": "application/json",
-          Authorization: "Bearer " + String(authTokens.access),
-        },
-      }
-    );
-    let data = await response.json();
-    if (response.status === 200) {
-      setIsLoading(false);
-      setMessages(data);
-    } else if (response.statusText === "Unauthorized") {
-      logoutUser();
+  // Get latest messages
+  const { response, isLoading, error } = useAxios({
+    method: "get",
+    url: `/chat/api/my-messages/${user.user_id}/`,
+    headers: {
+      "Content-Type": "application/json",
+      Authorization: "Bearer " + String(authTokens.access),
+    },
+  });
+
+  useEffect(() => {
+    if (response) {
+      setMessages(response);
+      setLoading(isLoading);
     }
-  };
+  }, [response]);
 
   const handleItemClick = (message) => {
     setSelectedProfile(null);
@@ -73,29 +66,128 @@ const HomePage = () => {
   const handleSendMessageFromModal = (e) => {
     setChatParticipants(null);
     setSelectedProfile(e);
-    console.log(selectedProfile);
   };
+
+  if (isLoading) {
+    return <Loading />;
+  }
 
   return (
     <div className="container-fluid">
-      {isLoading ? (
-        <Loading />
-      ) : (
-        <div className="row">
-          {isSmallScreen ? (
+      <div className="row">
+        {isSmallScreen ? (
+          <div className="col">
+            {/* Small screen (profile selected) */}
+            {selectedProfile && (
+              <InboxPage
+                key={selectedProfile.id}
+                participantID={selectedProfile.user}
+                onSelect={handleBackToList}
+                chatParticipants={chatParticipants}
+                participantProfile={selectedProfile}
+              />
+            )}
+            {/* Small screen (profile not selected) */}
+            {chatParticipants && !selectedProfile ? (
+              <InboxPage
+                key={chatParticipants.id}
+                participantID={
+                  chatParticipants.sender.id !== user.user_id
+                    ? chatParticipants.sender.id
+                    : chatParticipants.receiver.id
+                }
+                onSelect={handleBackToList}
+                chatParticipants={chatParticipants}
+              />
+            ) : (
+              <>
+                {/* Small screen, (no profile, no inbox selected ) */}
+                {!selectedProfile && (
+                  <ul className="list-group inbox-page">
+                    <h3>Chats</h3>
+                    <SearchUser onSearchResult={handleSearchResult} />
+                    {searchResult ? (
+                      <SearchResults
+                        searchResults={searchResult}
+                        onSendMessageFromModal={handleSendMessageFromModal}
+                      />
+                    ) : (
+                      latestMessages.map((message) => (
+                        <MessageListItem
+                          key={message.id}
+                          message={message}
+                          user={user}
+                          unread={message.unread_count}
+                          onClick={() => {
+                            handleItemClick(message);
+                          }}
+                        />
+                      ))
+                    )}
+                  </ul>
+                )}
+                {searchResult && !selectedProfile && (
+                  <button
+                    className="btn btn-outline-info"
+                    onClick={handleCloseSearchResult}
+                  >
+                    Back to my chats
+                  </button>
+                )}
+              </>
+            )}
+          </div>
+        ) : (
+          <>
+            {/* Default screen */}
             <div className="col">
-              {/* Small screen (profile selected) */}
+              <h3>Chats</h3>
+              <SearchUser onSearchResult={handleSearchResult} />
+              {searchResult ? (
+                <>
+                  <div className="inbox-page">
+                    <SearchResults
+                      searchResults={searchResult}
+                      onSendMessageFromModal={handleSendMessageFromModal}
+                    />
+                  </div>
+                  <button
+                    className="btn btn-outline-info mt-2"
+                    onClick={handleCloseSearchResult}
+                  >
+                    Back to my chats
+                  </button>
+                </>
+              ) : (
+                <>
+                  {/* Latest latestMessages */}
+                  <ul className="list-group inbox-page">
+                    {latestMessages.map((message) => (
+                      <MessageListItem
+                        key={message.id}
+                        message={message}
+                        user={user}
+                        unread={message.unread_count}
+                        onClick={() => handleItemClick(message)}
+                      />
+                    ))}
+                  </ul>
+                </>
+              )}
+            </div>
+            <div className="col-9">
+              {/* Profile selected */}
               {selectedProfile && (
                 <InboxPage
                   key={selectedProfile.id}
-                  participantID={selectedProfile.user}
+                  participantID={selectedProfile.user.id}
                   onSelect={handleBackToList}
                   chatParticipants={chatParticipants}
                   participantProfile={selectedProfile}
                 />
               )}
-              {/* Small screen (profile not selected) */}
-              {chatParticipants && !selectedProfile ? (
+              {/* Profile not selected */}
+              {chatParticipants && (
                 <InboxPage
                   key={chatParticipants.id}
                   participantID={
@@ -105,113 +197,13 @@ const HomePage = () => {
                   }
                   onSelect={handleBackToList}
                   chatParticipants={chatParticipants}
+                  participantProfile={null}
                 />
-              ) : (
-                <>
-                  {/* Small screen, (no profile, no inbox selected ) */}
-                  {!selectedProfile && (
-                    <ul className="list-group inbox-page">
-                      <h3>Chats</h3>
-                      <SearchUser onSearchResult={handleSearchResult} />
-                      {searchResult ? (
-                        <SearchResults
-                          searchResults={searchResult}
-                          onSendMessageFromModal={handleSendMessageFromModal}
-                        />
-                      ) : (
-                        messages.map((message) => (
-                          <MessageListItem
-                            key={message.id}
-                            message={message}
-                            user={user}
-                            unread={message.unread_count}
-                            onClick={() => {
-                              handleItemClick(message);
-                            }}
-                          />
-                        ))
-                      )}
-                    </ul>
-                  )}
-                  {searchResult && !selectedProfile && (
-                    <button
-                      className="btn btn-outline-info"
-                      onClick={handleCloseSearchResult}
-                    >
-                      Back to my chats
-                    </button>
-                  )}
-                </>
               )}
             </div>
-          ) : (
-            <>
-              {/* Default screen */}
-              <div className="col">
-                <h3>Chats</h3>
-                <SearchUser onSearchResult={handleSearchResult} />
-                {searchResult ? (
-                  <>
-                    <div className="inbox-page">
-                      <SearchResults
-                        searchResults={searchResult}
-                        onSendMessageFromModal={handleSendMessageFromModal}
-                      />
-                    </div>
-                    <button
-                      className="btn btn-outline-info mt-2"
-                      onClick={handleCloseSearchResult}
-                    >
-                      Back to my chats
-                    </button>
-                  </>
-                ) : (
-                  <>
-                    {/* Latest messages */}
-                    <ul className="list-group inbox-page">
-                      {messages.map((message) => (
-                        <MessageListItem
-                          key={message.id}
-                          message={message}
-                          user={user}
-                          unread={message.unread_count}
-                          onClick={() => handleItemClick(message)}
-                        />
-                      ))}
-                    </ul>
-                  </>
-                )}
-              </div>
-              <div className="col-9">
-                {/* Profile selected */}
-                {selectedProfile && (
-                  <InboxPage
-                    key={selectedProfile.id}
-                    participantID={selectedProfile.user.id}
-                    onSelect={handleBackToList}
-                    chatParticipants={chatParticipants}
-                    participantProfile={selectedProfile}
-                  />
-                )}
-                {/* Profile not selected */}
-                {chatParticipants && (
-                  <InboxPage
-                    key={chatParticipants.id}
-                    participantID={
-                      chatParticipants.sender.id !== user.user_id
-                        ? chatParticipants.sender.id
-                        : chatParticipants.receiver.id
-                    }
-                    onSelect={handleBackToList}
-                    chatParticipants={chatParticipants}
-                    participantProfile={null}
-                  />
-                )}
-              </div>
-            </>
-          )}
-        </div>
-      )}
+          </>
+        )}
+      </div>
     </div>
   );
 };
